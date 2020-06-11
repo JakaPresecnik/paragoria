dc21b893a1b7af7d78fdcca19686bbd5b7a2fe86
Jake <jaka.presecnik@gmail.com>
2020-06-11 13:55:39 +0200

    added sending mail(to my mail ATM)

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,20 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// mail nastavljen na emailjs.com
 
 function Booking () {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  // emailjs.com
+  const sendFeedback = (templateId, variables) => {
+    window.emailjs.send(
+  	'gmail', templateId,
+  	variables
+  	).then(res => {
+      alert('Message sent!')
+    	console.log('Email successfully sent!')
+  	})
+    .catch(err => {
+      console.error('Oh well, you failed. Here some thoughts on the error that occured:', err)
+      alert('Something went wrong.')
+    })
+  }
+
+  const clearAll = (e) => {
+    e.preventDefault()
+    setName('');
+    setSurname('');
+    setEmail('');
+    setMessage('');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const templateId = 'template_VOxnYylt';
+
+    sendFeedback(templateId, {
+      message_html: message,
+      name: name,
+      surname: surname,
+      email: email,
+    })
+    clearAll(e)
+  }
+
     return (
         <section className='booking'>
             <div className='section-header'>
                 <h2>BOOKING AND LABEL CONTACT</h2>
                 <p className='sub-text'> Just send us a message here and we will respond back to you as soon as possible!</p>
             </div>
-            
-            <form method="post" action="sendemail.php">
-                <input type='text' name='first_name' placeholder='Name' />
-                <input type='text' name='last_name' placeholder='Surname' />
-                <input type='text' name='email' placeholder='Email' />
-                <textarea name='message' placeholder='Describe your wishes / interests...' />
-                <button>SEND</button>
-                <button>CLEAR</button>
+            <form>
+                <input onChange={(e) => {setName(e.target.value)}}
+                  type='text'
+                  value={name}
+                  name='name'
+                  placeholder='Name'
+                  required />
+                <input onChange={(e) => {setSurname(e.target.value)}}
+                  type='text'
+                  value={surname}
+                  name='surname'
+                  placeholder='Surname'
+                  required />
+                <input onChange={(e) => {setEmail(e.target.value)}}
+                  type='text'
+                  value={email}
+                  name='email'
+                  placeholder='Email'
+                  required />
+                <textarea onChange={(e) => {setMessage(e.target.value)}}
+                  value={message}
+                  name='message'
+                  placeholder='Describe your wishes / interests...'
+                  required />
+                <button onClick={e => handleSubmit(e)} disabled={!name || !surname || !email || !message}>SEND</button>
+                <button onClick={e => clearAll(e)} disabled={!name && !surname && !email && !message}>CLEAR</button>
             </form>
             <div className='section-footer'>
                 <h4>PARAGORIA BOOKING AND LABEL CONTACT</h4>
