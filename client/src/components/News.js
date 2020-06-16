8e7c35362ca1acfc9d45dc08e20d2d6c9e419c2b
Jake <jaka.presecnik@gmail.com>
2020-06-16 19:11:07 +0200

    done the server getting data

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
+import ReactLoading from "react-loading";
 
 class News extends Component {
     state = {
-        data: [{
-            type: 'video',
-            key: '1',
-            date: '25/12/2019',
-            linkUrl: 'https://www.youtube.com/watch?v=PsLf10FESl4',
-            description: '6 years after Decomposition of mind we are back in studio recording our 2nd album. Album will be out in the middle of 2020.',
-          },
-          {
-            type: 'flyer',
-            key: '2',
-            date: '22/2/2019',
-            linkUrl: 'https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/51814874_783791751995120_2480949100749520896_o.jpg?_nc_cat=109&_nc_sid=b386c4&_nc_ohc=fTBtA4m2njgAX8a2gv6&_nc_ht=scontent-frt3-2.xx&oh=6aee565531374730de5020512d4f3c6f&oe=5F0551DC',
-            description: 'Camel Metal Show !! Gonoba - 15. obletnica in zadnji koncert - spet! Skupaj z gosti Paragoria in Licence to Hate !!',
-          },]
+        data: null,
+        loaded: false,
     }
 
     retrieveNews = async () => {
@@ -32,51 +21,58 @@ class News extends Component {
     componentDidMount() {
         this.retrieveNews()
         .then(res => this.setState({data: res}))
+        .then(() => this.setState({loaded: true}))
     }
 
     render() {
-        const { data } = this.state;
+        const { data, loaded } = this.state;
 
-        return (
-            <section>
-                <div className='section-header'>
-                    <h2>NEWS AND UPDATES</h2>
-                </div>
-
-                {data.map((news) => {
-                    if(news.type === 'video') {
-                        return (
-                            <div key={news.key} className='news' >
-                                <p className='date'>{news.date}</p>
-                                <p>{news.description}</p>
-                                <div className='news-video'>
-                                <ReactPlayer className='news-video-box' url={news.linkUrl} />
+        if(loaded === true) {
+            return (
+                <section>
+                    <div className='section-header'>
+                        <h2>NEWS AND UPDATES</h2>
+                    </div>
+    
+                    {data.map((news) => {
+                        if(news.type === 'video') {
+                            return (
+                                <div key={news.key} className='news' >
+                                    <p className='date'>{news.date}</p>
+                                    <p>{news.description}</p>
+                                    <div className='news-video'>
+                                    <ReactPlayer className='news-video-box' url={news.linkUrl} />
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    }else if(news.type === 'flyer') {
-                        return (
-                            <div  key={news.key} className='news'>
-                                <p className='date'>{news.date}</p>
-                                <a href={news.linkUrl}>
-                                <img className='news-image' src={news.linkUrl} alt='News banner' />
-                                </a>
-                                <p>{news.description}</p>
-                            </div>
-                        )
-                    }else {
-                        return (
-                            <div  key={news.key} className='news'>
-                                <p className='date'>{news.date}</p>
-                                <h3>{news.header}</h3>
-                                <p>{news.description}</p>
-                            </div>
-                        )
-                    }
-                })}
-                
-            </section>
-        )
+                            )
+                        }else if(news.type === 'flyer') {
+                            return (
+                                <div  key={news.key} className='news'>
+                                    <p className='date'>{news.date}</p>
+                                    <a href={news.linkUrl}>
+                                    <img className='news-image' src={news.linkUrl} alt='News banner' />
+                                    </a>
+                                    <p>{news.description}</p>
+                                </div>
+                            )
+                        }else {
+                            return (
+                                <div  key={news.key} className='news'>
+                                    <p className='date'>{news.date}</p>
+                                    <h3>{news.header}</h3>
+                                    <p>{news.description}</p>
+                                </div>
+                            )
+                        }
+                    })}
+                    
+                </section>
+            )
+        }else {
+            return (
+                <ReactLoading type={"bars"} color={"#632828"} width={230} className='loading'/>
+            )
+        }
     }
 }
 export default News;
