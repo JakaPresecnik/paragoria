59922eda71bcf3e9c6e091bde02df689ee71d8c8
Jake <jaka.presecnik@gmail.com>
2020-06-18 15:39:59 +0200

    navbar fixed and change on scroll

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function Nav () {
-    return (
-        <nav>
-            <ul>
-                <li><NavLink to='/paragoria' exact activeClassName="selected">Home</NavLink></li>
-                <li><NavLink to='/paragoria/news' activeClassName="selected">News</NavLink></li>
-                <li><NavLink to='/paragoria/visuals' activeClassName="selected">Visuals</NavLink></li>
-                <li><NavLink to='/paragoria/biography' activeClassName="selected">Biography</NavLink></li>
-                <li><NavLink to='/paragoria/booking' activeClassName="selected">Booking</NavLink></li>         
-            </ul>
-        </nav>
-    )
+class Nav extends React.Component {
+    state = {
+        shadeNavigation: false,
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 30) {
+                this.setState({shadeNavigation: true})
+            }else {
+                this.setState({shadeNavigation: false})
+            }
+        })
+    }
+    render () {
+        return (
+            <nav>
+                <ul className={this.state.shadeNavigation ? 'scrolled' : null}>
+                    <li><NavLink to='/paragoria' exact activeClassName="selected">Home</NavLink></li>
+                    <li><NavLink to='/paragoria/news' activeClassName="selected">News</NavLink></li>
+                    <li><NavLink to='/paragoria/visuals' activeClassName="selected">Visuals</NavLink></li>
+                    <li><NavLink to='/paragoria/biography' activeClassName="selected">Biography</NavLink></li>
+                    <li><NavLink to='/paragoria/booking' activeClassName="selected">Booking</NavLink></li>         
+                </ul>
+            </nav>
+        )
+    }
 }
 
 export default Nav
