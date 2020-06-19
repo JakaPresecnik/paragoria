807a9af374a6e9f6f849a6ae22e64b567e6766b1
Jake <jaka.presecnik@gmail.com>
2020-06-19 18:43:24 +0200

    edited footer and applied some additional stylig on navbar

--- client/src/App.js
+++ client/src/App.js
@@ -14,30 +14,48 @@ import Visuals from './components/Visuals';
 
 import AddNews from './components/AddNews';
 
-function App () {
- return ( 
-  <div className='container'>
-    <div className='inner-container'>
-      <div className='homepage-container'>
-        <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route path='/paragoria' exact component={Header} />
-            <Route path='/paragoria/news' component={News} />
-            <Route path='/paragoria/biography' component={Biography} />
-            <Route path='/paragoria/visuals' component={Visuals} />
-            <Route path='/paragoria/booking' component={Booking} />
+class App extends React.Component {
+  state = {
+      shadeNavigation: false,
+  }
 
-            <Route path='/paragoria/addNews' component={AddNews} />
-          </Switch>
-          <ReactPlayer url='https://soundcloud.com/on-parole/paragoria-from-carcass-to-soil' playing={true} width='70%' height='70px' style={{margin: '2em auto', maxWidth: '500px'}}/>
-          <Footer />
-        </BrowserRouter>
-        
-      </div>
-    </div>
-  </div>
-  )
+  componentDidMount() {
+      window.addEventListener('scroll', () => {
+          if(window.scrollY > 30) {
+              this.setState({shadeNavigation: true})
+          }else {
+              this.setState({shadeNavigation: false})
+          }
+      })
+  }
+
+  render () {
+    return (
+     <div className='container'>
+       <div className='inner-container'>
+         <div className='homepage-container'>
+           <BrowserRouter>
+             <Nav shadeNavigation={this.state.shadeNavigation} />
+             <Switch>
+               <Route path='/paragoria' exact render={() => (
+                 <Header shadeNavigation={this.state.shadeNavigation}/>
+               )} />
+               <Route path='/paragoria/news' component={News} />
+               <Route path='/paragoria/biography' component={Biography} />
+               <Route path='/paragoria/visuals' component={Visuals} />
+               <Route path='/paragoria/booking' component={Booking} />
+
+               <Route path='/paragoria/addNews' component={AddNews} />
+             </Switch>
+             <ReactPlayer url='https://soundcloud.com/on-parole/paragoria-from-carcass-to-soil' playing={false} width='70%' height='70px' style={{margin: '2em auto', maxWidth: '500px'}}/>
+             <Footer shadeNavigation={this.state.shadeNavigation} />
+           </BrowserRouter>
+
+         </div>
+       </div>
+     </div>
+     )
+  }
 }
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { FaSoundcloud, FaAmazon } from 'react-icons/fa'
 
 import logo from '../img/logo.png';
 import cd from '../img/last_album.png';
 
-function Header () {
-    return (
+class Header extends React.Component {
+    state = {
+        popUp: true,
+    }
+
+    handleShowBox() {
+        this.setState((prevState) => ({popUp: !prevState.popUp}))
+    }
+
+    render() {
+        return (
             <header>
                 <img src={logo} alt='logo' />
-                <img src={cd} alt='Album cover' />
+                <div className={this.state.popUp ? 'hiddenBox' : 'buyAlbum'}>
+                    <p>Buy it on:</p>
+                    <ul>
+                        <li><a className={this.props.shadeNavigation ? 'scrolled-link' : null} href='https://soundcloud.com/paragoria' target='_blank' rel="noopener noreferrer"><FaSoundcloud size='1.5em' /></a></li>
+                        <li><a className={this.props.shadeNavigation ? 'scrolled-link' : null} href='https://www.amazon.com/Decomposition-Mind-Paragoria/dp/B00HQVNB84' target='_blank' rel="noopener noreferrer"><FaAmazon size='1.5em' /></a></li>
+                    </ul>
+                </div>
+                <img className='albumCover' src={cd} alt='Album cover' onClick={() => this.handleShowBox()}/>
             </header>
     )
+    }
 }
 
 export default Header;
