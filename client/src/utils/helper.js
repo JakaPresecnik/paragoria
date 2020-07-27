4b89a7366cbf919044dc6b92a70099b3854b416f
Jake <jaka.presecnik@gmail.com>
2020-07-27 16:29:18 +0200

    new photos

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,25 +1,29 @@
-import para from '../img/album/para.jpg'
-import paraa from '../img/album/para1.jpg'
-import parab from '../img/album/para2.jpg'
-import parac from '../img/album/para3.jpg'
-import parad from '../img/album/para4.jpg'
+import para from '../img/album/para.JPG'
+import paraa from '../img/album/para1.JPG'
+import parab from '../img/album/para2.JPG'
+import parac from '../img/album/para3.JPG'
+import parad from '../img/album/para4.JPG'
+import parae from '../img/album/para5.JPG'
 import live from '../img/album/live.jpg'
 import livea from '../img/album/live1.jpg'
 import liveb from '../img/album/live2.jpg'
 import livec from '../img/album/live3.jpg'
 import lived from '../img/album/live4.jpg'
+import livef from '../img/album/live6.jpg'
+import liveg from '../img/album/live7.jpg'
+import liveh from '../img/album/live8.jpg'
 
 const images = [
     {
         src: para,
-        width: 2,
-        height: 3,
+        width: 3,
+        height: 2,
         type: 'shoot'
       },
       {
         src: paraa,
-        width: 1,
-        height: 1,
+        width: 3,
+        height: 2,
         type: 'shoot'
       },
       {
@@ -36,8 +40,14 @@ const images = [
       },
       {
         src: parad,
-        width: 3,
-        height: 2,
+        width: 5,
+        height: 3,
+        type: 'shoot'
+      },
+      {
+        src: parae,
+        width: 5,
+        height: 3,
         type: 'shoot'
       },
       {
@@ -60,12 +70,30 @@ const images = [
       },
       {
         src: livec,
+        width: 3,
+        height: 2,
+        type: 'live'
+      },
+      {
+        src: lived,
         width: 2,
         height: 3,
         type: 'live'
       },
       {
-        src: lived,
+        src: livef,
+        width: 3,
+        height: 2,
+        type: 'live'
+      },
+      {
+        src: liveg,
+        width: 3,
+        height: 2,
+        type: 'live'
+      },
+      {
+        src: liveh,
         width: 3,
         height: 2,
         type: 'live'
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const newsData = [
     type: 'flyer',
     key: '2',
     date: '22. FEBRUARY 2019',
-    linkUrl: 'https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/51814874_783791751995120_2480949100749520896_o.jpg?_nc_cat=109&_nc_sid=b386c4&_nc_ohc=fTBtA4m2njgAX8a2gv6&_nc_ht=scontent-frt3-2.xx&oh=6aee565531374730de5020512d4f3c6f&oe=5F0551DC',
+    linkUrl: 'https://i.ibb.co/fXb1Cpv/flyer.jpg',
     description: 'Camel Metal Show !! Gonoba - 15. obletnica in zadnji koncert - spet! Skupaj z gosti Paragoria in Licence to Hate !!',
   },
   {
