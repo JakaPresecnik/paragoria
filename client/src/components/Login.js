bce077880cc4cd8bab2691676b3b1adf32aedb33
Jake <jaka.presecnik@gmail.com>
2020-09-07 06:49:54 +0200

    fix set up for official update

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -48,16 +48,6 @@ const Login = (props) => {
                         className='preview-btn' 
                         style={{marginTop: '2em'}}
                     >LOGIN</button>
-                    <button 
-                        onClick={(e) => {
-                            e.preventDefault()
-                            auth.register(inputs, () => {
-                                props.history.push('/edit');
-                            })
-                        }}
-                        className='preview-btn' 
-                        style={{marginTop: '2em'}}
-                    >REGISTER</button>
                 </form>
             </div>
         </section>
